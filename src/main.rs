b8b02c5344d4eaa776b96f740db04c837e648d48
dhanushl0l <dhanushdjtherock@gmail.com>
2025-05-09 01:12:19 +0530

    admin page and itme list FIX

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     env,
-    fs::{self, File},
+    fs::{self, File, create_dir_all},
     io::{Cursor, Read, Write},
     path::{Path, PathBuf},
 };
@@ -11,9 +11,8 @@ use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, get, web}
 use futures_util::StreamExt;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-use zip::read::ZipArchive;
 
-const PASS: &str = "pass";
+const PASS: Option<&str> = option_env!("PASS");
 
 async fn serve_page(req: HttpRequest) -> impl Responder {
     let page = req.match_info().query("filename");
@@ -35,6 +34,7 @@ async fn serve_page(req: HttpRequest) -> impl Responder {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>{}</title>
             <style>{}</style>
+            <style>{}</style>
         </head>
         <body>
             <nav>
@@ -50,6 +50,7 @@ async fn serve_page(req: HttpRequest) -> impl Responder {
         "#,
         page.to_uppercase(),
         &fs::read_to_string("static/style.css").unwrap(),
+        &fs::read_to_string("static/colors.css").unwrap(),
         content
     );
 
@@ -61,10 +62,11 @@ async fn list_files(
 ) -> impl Responder {
     let pass = query.get("pass").unwrap_or(&"".to_string()).clone();
 
-    let path = PathBuf::from("static/items");
+    let path = PathBuf::from(DATABASE_PATH);
     match fs::read_dir(&path) {
         Ok(entries) => {
-            if pass == PASS {
+            println!("{}||{}", pass, PASS.unwrap());
+            if pass == PASS.unwrap() {
                 let files: Vec<String> = entries
                     .filter_map(|entry| entry.ok())
                     .filter_map(|entry| entry.file_name().into_string().ok())
@@ -87,7 +89,7 @@ async fn delete_file(
 
     let pass = query.get("pass").unwrap_or(&"".to_string()).clone();
 
-    if pass == PASS {
+    if pass == PASS.unwrap() {
         if file_path.exists() {
             match fs::remove_file(&file_path) {
                 Ok(_) => HttpResponse::Ok().body(format!("{} deleted successfully", filename)),
@@ -106,132 +108,172 @@ async fn entry() -> impl Responder {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct FormData {
-    pub firstname: String,
-    description: String,
-    price: f64,
-    weight: f64,
-    r#type: String,
-    rank: i32,
-    securitykey: String,
-}
-
-#[derive(Debug, Serialize)]
 struct SanitizedFormData {
     firstname: String,
     description: String,
-    price: f64,
-    weight: f64,
-    r#type: String,
+    price: i32,
+    weight: i32,
+    typee: String,
     rank: i32,
+    url: String,
 }
 
-const SECURITY_KEY: &str = "1";
+const DATABASE_PATH: &str = "database";
 
-async fn submit(mut payload: Multipart) -> impl Responder {
-    println!("Received a request to /submit");
+async fn submit(
+    mut payload: Multipart,
+    query: web::Query<std::collections::HashMap<String, String>>,
+) -> HttpResponse {
+    let key = query.get("key").cloned().unwrap_or_default();
 
-    let mut form_data = None;
-    let mut images = vec![];
-
-    while let Some(item) = payload.next().await {
-        if let Ok(mut field) = item {
-            if field.name().unwrap() == "data" {
-                // Handle form data (JSON)
-                let mut field_data = Vec::new();
-                while let Some(chunk) = field.next().await {
-                    if let Ok(data) = chunk {
-                        field_data.extend_from_slice(&data);
-                    }
+    if &key == PASS.unwrap() {
+        while let Some(item) = payload.next().await {
+            let mut field = match item {
+                Ok(f) => f,
+                Err(e) => {
+                    eprintln!("Multipart error: {:?}", e);
+                    return HttpResponse::BadRequest().body("Invalid multipart data");
                 }
-                let json_str = String::from_utf8(field_data).unwrap();
-                form_data = Some(serde_json::from_str::<FormData>(&json_str).unwrap());
-            } else if field.name().unwrap().starts_with("image") {
-                // Handle file uploads (images)
-                let mut file_data = Vec::new();
-                while let Some(chunk) = field.next().await {
-                    if let Ok(data) = chunk {
-                        file_data.extend_from_slice(&data);
-                    }
+            };
+
+            let content_disposition = field.content_disposition();
+            let field_name = content_disposition.unwrap().get_name().unwrap_or_default();
+            if field_name != "file" {
+                continue;
+            }
+
+            let mut data = web::BytesMut::new();
+            while let Some(chunk) = field.next().await {
+                data.extend_from_slice(&chunk.unwrap());
+            }
+
+            let cursor = Cursor::new(data);
+            let mut zip = ZipArchive::new(cursor).unwrap();
+
+            let mut data: Option<SanitizedFormData> = None;
+            let mut images = Vec::new();
+            for i in 0..zip.len() {
+                let mut file = zip.by_index(i).unwrap();
+                let name = file.name().to_string();
+
+                if name.ends_with('/') {
+                    continue;
                 }
-                images.push(file_data);
+
+                if name == "data.json" {
+                    let mut json_string = String::new();
+                    file.read_to_string(&mut json_string).unwrap();
+                    data = serde_json::from_str(&json_string).unwrap();
+                    println!("{:?}", data);
+                } else if name.starts_with("images/") {
+                    let filename = name
+                        .strip_prefix("images/")
+                        .unwrap_or("image.png")
+                        .to_string();
+                    let mut buffer = Vec::new();
+                    file.read_to_end(&mut buffer).unwrap();
+                    images.push((filename, buffer));
+                }
+            }
+
+            if let Some(data) = data {
+                let mut path = PathBuf::from(DATABASE_PATH);
+                path.push(&data.firstname);
+                std::fs::create_dir_all(&path).unwrap();
+
+                path.push("user.json");
+                let json = serde_json::to_string_pretty(&data).unwrap();
+                fs::write(&path, json).unwrap();
+                path.pop();
+                path.push("image");
+                fs::create_dir_all(&path).unwrap();
+                for (name, data) in images {
+                    path.push(&name);
+                    std::fs::create_dir_all(path.parent().unwrap()).unwrap();
+                    let mut file = File::create(&path).unwrap();
+                    file.write_all(&data).unwrap();
+                    path.pop();
+                }
+            } else {
+                return HttpResponse::InternalServerError().body("Error parsing json");
             }
         }
+    } else {
+        return HttpResponse::Unauthorized().body("invalid password");
     }
 
-    if let Some(data) = form_data {
-        let name = data.firstname.clone();
-        let dir_path = format!("./{}/images", name);
-        fs::create_dir_all(&dir_path).unwrap();
-
-        for (i, image_data) in images.into_iter().enumerate() {
-            let image_path = format!("{}/image{}.jpg", dir_path, i + 1);
-            let mut image_file = File::create(&image_path).unwrap();
-            image_file.write_all(&image_data).unwrap();
-        }
-
-        // Optionally, save JSON with sanitized data
-        let sanitized_data = format!(
-            "{{\"name\":\"{}\",\"description\":\"{}\",\"price\":{}}}",
-            data.firstname, data.description, data.price
-        );
-        let json_path = format!("./{}/data.json", name);
-        fs::write(json_path, sanitized_data).unwrap();
-
-        return HttpResponse::Ok()
-            .json(json!({ "message": "File uploaded and form processed successfully!" }));
-    }
-
-    HttpResponse::BadRequest().json(json!({ "message": "Invalid file upload or form data." }))
+    HttpResponse::Ok().body("Submission received and processed")
 }
 
-#[derive(Serialize)]
-struct Product {
-    name: String,
-    price: String,
-    old_price: String,
-    banner: String,
+#[derive(Debug, Serialize, Deserialize)]
+struct Prodects {
+    firstname: String,
+    description: String,
+    price: i32,
+    weight: i32,
+    typee: String,
+    rank: i32,
+    url: String,
     images: Vec<String>,
 }
 
+impl Prodects {
+    fn from(data: SanitizedFormData, images: Vec<String>) -> Self {
+        Self {
+            firstname: data.firstname,
+            description: data.description,
+            price: data.price,
+            weight: data.weight,
+            typee: data.typee,
+            rank: data.rank,
+            url: data.url,
+            images,
+        }
+    }
+}
 async fn get_products() -> impl Responder {
-    let common_images = vec![
-        "static/img/img1.jpg".into(),
-        "static/img/img2.jpg".into(),
-        "static/img/img3.jpg".into(),
-    ];
+    let mut products = Vec::new();
 
-    let products = vec![
-        Product {
-            name: "Product 1".into(),
-            price: "₹26,999".into(),
-            old_price: "₹29,999".into(),
-            banner: "static/img/img1.jpg".into(),
-            images: common_images.clone(),
-        },
-        Product {
-            name: "Product 2".into(),
-            price: "₹19,999".into(),
-            old_price: "₹24,999".into(),
-            banner: "static/img/img1.jpg".into(),
-            images: common_images.clone(),
-        },
-    ];
+    if let Ok(entries) = fs::read_dir(DATABASE_PATH) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                let mut path = entry.path();
+                path.push("user.json");
+                let file = fs::read_to_string(&path).unwrap();
+                let data: SanitizedFormData = serde_json::from_str(&file).unwrap();
+                let mut prod_images = Vec::new();
+                path.pop();
+                path.push("image");
+                if let Ok(images) = fs::read_dir(path) {
+                    for image in images {
+                        if let Ok(image) = image {
+                            let image: String = image.path().to_string_lossy().into_owned();
+                            prod_images.push(image);
+                        }
+                    }
+                }
+                let data = Prodects::from(data, prod_images);
+                products.push(data);
+            }
+        }
+    }
 
     HttpResponse::Ok().json(products)
 }
 
 use env_logger::{self, Env};
 use log::info;
+use zip::ZipArchive;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::Builder::from_env(Env::default().filter_or("LOG", "info")).init();
 
-    info!("Starting server on http://127.0.0.1:8080");
+    info!("Starting server on http://127.0.0.1:8081");
 
     HttpServer::new(|| {
         App::new()
+            .service(afs::Files::new("/database", "./database").show_files_listing())
             .service(afs::Files::new("/static", "static").show_files_listing())
             .route("/files", web::get().to(list_files))
             .route("/delete/{filename}", web::delete().to(delete_file))
