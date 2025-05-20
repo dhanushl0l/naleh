610818d03d0cf469ae9e067b77b79557f43ee1c6
dhanushl0l <dhanushdjtherock@gmail.com>
2025-05-21 02:02:16 +0530

    update

--- src/main.rs
+++ src/main.rs
@@ -39,9 +39,9 @@ async fn serve_page(req: HttpRequest) -> impl Responder {
         <body>
             <nav>
                 <ul>
-                    <li><a href="/home">Home</a></li>
-                    <li><a href="/services">Services</a></li>
-                    <li><a href="/about">About</a></li>
+                    <li><a href="/home"><img src="static/img/home.svg" alt="Home"><span>Home</span></a></li>
+                    <li><a href="/services"><img src="static/img/cart.svg" alt="Services"><span>Shop</span></a></li>
+                    <li><a href="/about"><img src="static/img/about.svg" alt="About"><span>About</span></a></li>
                 </ul>
             </nav>
         {}
